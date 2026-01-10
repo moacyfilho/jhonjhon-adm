0228c36f99dbb1c87e4cfc3a19303949f99ec427
Moacy Soares <moacysoares@gmail.com>
2026-01-10 12:08:22 -0400

    fix: hardcode keys in middleware too

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -9,8 +9,8 @@ export async function updateSession(request: NextRequest) {
     })
 
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        'https://tmhokhxuavrglpnuorrg.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtaG9raHh1YXZyZ2xwbnVvcnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDI1MTYsImV4cCI6MjA4MzQ3ODUxNn0.8ku7FqoszTVqdF6YE8GXsNocT4Xf5ofThr1hlu8I_Jo',
         {
             cookies: {
                 get(name: string) {
