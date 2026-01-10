13e2219a42c8bc25df42b70dc46577aa1fb57330
Moacy Soares <moacysoares@gmail.com>
2026-01-10 12:02:31 -0400

    fix: hardcode supabase keys to prevent build error

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,9 @@
+
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
     return createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        'https://tmhokhxuavrglpnuorrg.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtaG9raHh1YXZyZ2xwbnVvcnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDI1MTYsImV4cCI6MjA4MzQ3ODUxNn0.8ku7FqoszTVqdF6YE8GXsNocT4Xf5ofThr1hlu8I_Jo'
     )
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -6,8 +6,8 @@ export function createClient() {
 
     // Create a server-side Supabase client with the compiler to use cookies
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        'https://tmhokhxuavrglpnuorrg.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtaG9raHh1YXZyZ2xwbnVvcnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDI1MTYsImV4cCI6MjA4MzQ3ODUxNn0.8ku7FqoszTVqdF6YE8GXsNocT4Xf5ofThr1hlu8I_Jo',
         {
             cookies: {
                 get(name: string) {
